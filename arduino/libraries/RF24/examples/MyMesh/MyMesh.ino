e133ec1801491c3c17bdb5d11129b9ca8d0d3f99
Justin Shaw <wyojustin@gmail.com>
2016-11-11 20:03:15 -0500

    minor bugfix

--- arduino/libraries/RF24/examples/MyMesh/MyMesh.ino
+++ arduino/libraries/RF24/examples/MyMesh/MyMesh.ino
@@ -2,14 +2,15 @@
 #include "RF24.h"
 
 /****************** User Config ***************************/
-const int n_node = 7;
+const int n_node = 6;
 
-uint8_t myRadioNum = 0; // 0, 1, 2, 3, 4, 5, 6
+uint8_t myRadioNum = 0; // 0, 1, 2, 3, 4, 5
 
 RF24 radio(9, 10);
 
 // Radio pipe addresses for the 7 nodes to communicate.
-byte addresses[][6] = {"0Node", "1Node","2Node","3Node","4Node","5Node","6Node"};
+const byte NODE_NAME_LEN = 6;
+byte addresses[][NODE_NAME_LEN] = {"0Node", "1Node","2Node","3Node","4Node","5Node","6Node"};
 
 
 // A single byte to keep track of the data being sent back and forth
@@ -21,14 +22,14 @@ void setup(){
   radio.enableAckPayload();                     // Allow optional ack payloads
   radio.enableDynamicPayloads();                // Ack payloads are dynamic payloads
   
-  for(byte radioNum=0; radioNum < n_node; radioNum++){
+  for(byte radioNum=0; radioNum < 1; radioNum++){
     if(radioNum != myRadioNum){
       radio.openReadingPipe(radioNum,addresses[radioNum]);      // Open a reading pipe on address 0, pipe 1
     }
   }
-  radio.openWritingPipe(addresses[(3) % 6]);
   if(myRadioNum == 0){
-    while(1){
+    while(0){
+      radio.openWritingPipe(addresses[(3) % 6]);
       byte msg = 123;
       if(radio.write(&msg,1)){
 	Serial.println("Woot!");
@@ -43,19 +44,19 @@ void setup(){
 }
 
 void loop(){
-  unsigned long time = micros();                              // Record the current microsecond count   
   byte gotByte;                                               // Initialize a variable for the incoming response
   
   if(myRadioNum == 0){
     Serial.print("Now sending ");                             // Use a simple byte count as payload
     Serial.println(count);
     for(byte radioNum=1; radioNum < n_node; radioNum++){
+      unsigned long time = micros();                              // Record the current microsecond count   
       radio.stopListening();                                  // First, stop listening so we can talk.      
       radio.openWritingPipe(addresses[radioNum]);
       if(radio.write(&count,1)){                         // Send the count variable to the other radios
 	if(!radio.available()){                             // If nothing in the buffer, we got an ack but it is blank
 	  Serial.print(F("Got blank response. round-trip delay: "));
-	  Serial.print(micros()-time);
+	  Serial.print(micros() - time);
 	  Serial.println(F(" microseconds"));     
 	}
 	else{      
